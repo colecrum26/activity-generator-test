9765a6f502b2930350a27ae5b03fa8e18d0289e5
colecrum26 <cwcrumrine@gmail.com>
2023-04-12 15:22:24 -0500

    updaated README

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ## Wireframing
 ![website wireframe](/images/wireframe.png)
+This was my original design for the project. After delving further into the API documentation, I needed to adjust my proposal.
 
 ## Development Process
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -5,7 +5,8 @@ function Header(props) {
     return (
         <div>
             <h1>Bored?</h1>
-            <p>Let's find an activity for you.</p>
+            <h3>Let's find an activity for you.</h3>
+            <p>Click one of the following buttons to filter activities.</p>
             <Activities />
         </div>
     )
