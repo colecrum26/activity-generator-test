6c453fc5d0f5ef5c5429a82647a042a7ca669311
colecrum26 <cwcrumrine@gmail.com>
2023-04-18 09:47:03 -0500

    added musical component

--- client/src/categoryPages/index.js
+++ client/src/categoryPages/index.js
@@ -5,5 +5,15 @@ import DIY from "./diy";
 import Charitable from "./charitable";
 import Cooking from "./cooking";
 import Relaxation from "./relaxation";
+import Musical from "./musical";
 
-export { Educational, Recreational, Social, DIY, Charitable, Cooking, Relaxation };
+export {
+  Educational,
+  Recreational,
+  Social,
+  DIY,
+  Charitable,
+  Cooking,
+  Relaxation,
+  Musical
+};
