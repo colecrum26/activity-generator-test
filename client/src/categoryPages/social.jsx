e086a4685ba9669460c2366607252a63d6c2c9b4
colecrum26 <cwcrumrine@gmail.com>
2023-04-19 11:40:57 -0500

    refactored category NavLinks

--- client/src/categoryPages/social.jsx
+++ client/src/categoryPages/social.jsx
@@ -11,7 +11,7 @@ function Social() {
         return response.json();
       })
       .then((result) => {
-        console.log(result)
+        console.log(result);
         setData(result);
       })
       .catch((err) => {
@@ -22,24 +22,41 @@ function Social() {
 
   useEffect(() => {
     getActivity();
-  },[]);
+  }, []);
 
   return (
     <div>
-      <h2 id="activityHeader">Social Activity <img src={socIcon} className="icon" alt="people" /></h2>
+      <div>
+        <button
+          type="submit"
+          id="back"
+          style={{ backgroundColor: "transparent", borderColor: "transparent" }}
+        >
+          <NavLink to="/categories">
+            <img src={backIcon} alt="back button" />
+          </NavLink>
+        </button>
+        <h2 id="activityHeader">
+          Social Activity <img src={socIcon} className="icon" alt="people" />
+        </h2>
+      </div>
       <h4>Activity: {data.activity}</h4>
       <h4>Number of Participants: {data.participants}</h4>
-      <h4>Price: {data.price*10}</h4>
+      <h4>Price: {data.price * 10}</h4>
       <p>Price is scaled 0-10, with 0 being free.</p>
-      <h4>Accessibility: {data.accessibility*10}</h4>
+      <h4>Accessibility: {data.accessibility * 10}</h4>
       <p>Accessibility is scaled 0-10, with 0 being most accessible.</p>
       <div id="brBtns">
-      <button type="submit" id="back" style={{backgroundColor: "transparent", borderColor: "transparent"}}> 
-        <NavLink to="/categories"><img src={backIcon} alt="back button" /></NavLink>
-      </button>
-      <button onClick={getActivity} style={{backgroundColor: "transparent", borderColor: "transparent"}}><img src={refrIcon} alt="refresh" /></button>
+        <label htmlFor="refresh">Don't want to do this one? Try another activity!</label>
+        <button
+          className="refresh"
+          onClick={getActivity}
+          style={{ backgroundColor: "transparent", borderColor: "transparent" }}
+        >
+          <img src={refrIcon} alt="refresh" />
+        </button>
       </div>
-      </div>
+    </div>
   );
 }
 
--- client/src/components/categories.jsx
+++ client/src/components/categories.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import { 
+import {
   backIcon,
   eduIcon,
   recIcon,
@@ -9,65 +9,56 @@ import {
   cookIcon,
   relIcon,
   musIcon,
-  busyIcon, } from "../assets/index";
+  busyIcon,
+} from "../assets/index";
 
 function Categories() {
   return (
     <div id="categories">
       <header id="categoryHeader">
         <div id="topRow">
-      <button type="submit" id="home" 
-      style={{backgroundColor: "transparent", borderColor: "transparent"}}> 
-        <NavLink to="/"><img src={backIcon} alt="back button" /></NavLink>
-      </button>
-        <h2 id="AC">Activity Categories</h2>
+          <button
+            type="submit"
+            id="home"
+            style={{
+              backgroundColor: "transparent",
+              borderColor: "transparent",
+            }}
+          >
+            <NavLink to="/">
+              <img src={backIcon} alt="back button" />
+            </NavLink>
+          </button>
+          <h2 id="AC">Activity Categories</h2>
         </div>
         <div id="icons">
-        <img src={eduIcon} alt="books" />
-        <img src={recIcon} alt="bicycle" />
-        <img src={socIcon} alt="people" />
-        <img src={diyIcon} alt="tools" />
-        <img src={charityIcon} alt="cash" />
-        <img src={cookIcon} alt="utensils" />
-        <img src={relIcon} alt="battery" />
-        <img src={musIcon} alt="headphones" />
-        <img src={busyIcon} alt="clock" />
+          <img src={eduIcon} alt="books" />
+          <img src={recIcon} alt="bicycle" />
+          <img src={socIcon} alt="people" />
+          <img src={diyIcon} alt="tools" />
+          <img src={charityIcon} alt="cash" />
+          <img src={cookIcon} alt="utensils" />
+          <img src={relIcon} alt="battery" />
+          <img src={musIcon} alt="headphones" />
+          <img src={busyIcon} alt="clock" />
         </div>
         <h3>Let's find an activity for you.</h3>
         <h4>Choose a category you want to explore.</h4>
       </header>
-    <div id="categoryBtnsS1">
-      <button type="submit" id="educational">
-        <NavLink to="/educational">Educational</NavLink>
-      </button>
-      <button type="submit" id="recreational">
-        <NavLink to="/recreational">Recreational</NavLink>
-      </button>
-      <button type="submit" id="social">
-        <NavLink to="/social">Social</NavLink>
-      </button>
-      <button type="submit" id="diy">
-        <NavLink to="/diy">DIY</NavLink>
-      </button>
-      <button type="submit" id="charity">
-        <NavLink to="/charity">Charitable</NavLink>
-      </button>
-    </div>  
-    <div id="categoryBtnsS2">
-      <button type="submit" id="cooking">
-        <NavLink to="/cooking">Cooking</NavLink>
-      </button>
-      <button type="submit" id="relaxation">
-        <NavLink to="/relaxation">Relaxation</NavLink>
-      </button>
-      <button type="submit" id="music">
-        <NavLink to="/music">Musical</NavLink>
-      </button>
-      <button type="submit" id="busywork">
-        <NavLink to="/busywork">Busywork</NavLink>
-      </button>
+      <div id="categoryBtnsS1">
+          <NavLink to="/educational"> <button>Educational</button></NavLink>
+          <NavLink to="/recreational"> <button>Recreational</button> </NavLink>
+          <NavLink to="/social"> <button>Social</button> </NavLink>
+          <NavLink to="/diy"> <button>DIY</button> </NavLink>
+          <NavLink to="/charity"> <button>Charitable</button> </NavLink>
+      </div>
+      <div id="categoryBtnsS2">
+          <NavLink to="/cooking"> <button>Cooking</button> </NavLink>
+          <NavLink to="/relaxation"> <button>Relaxation</button> </NavLink>
+          <NavLink to="/music"> <button>Musical</button> </NavLink>
+          <NavLink to="/busywork"> <button>Busywork</button> </NavLink>
+      </div>
     </div>
-  </div>
   );
 }
 
