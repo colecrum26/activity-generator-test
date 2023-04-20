d484a17cadbe40bcd81043e055eb3b2c238a9904
colecrum26 <cwcrumrine@gmail.com>
2023-04-20 12:21:10 -0500

    trying something else

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/activity-generator/'
+  base: '/activity-generator/',
+  plugins: [react()]
 })
